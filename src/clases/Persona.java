f4805fb3733cae30dccab5367219d9857e64e7ac
Javier <javi_cero@hotmail.com>
2016-11-17 20:57:18 -0500

    Herencia aplicada

--- src/clases/Persona.java
+++ src/clases/Persona.java
@@ -5,6 +5,7 @@
  */
 package clases;
 
+import clases.*;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
@@ -12,42 +13,14 @@ import java.io.ObjectOutputStream;
  *
  * @author user
  */
-public class Persona implements java.io.Serializable {
-    
-    private String cedula;
-    private String nombre;
-    private String apellido;
+public class Persona extends PrincipalPersonaCliente{
+   
+
     private String sexo;
 
     public Persona(String cedula, String nombre, String apellido, String sexo) {
-        this.cedula = cedula;
-        this.nombre = nombre;
-        this.apellido = apellido;
+        super(cedula, nombre, apellido);
         this.sexo = sexo;
-    }
-
-    public String getCedula() {
-        return cedula;
-    }
-
-    public void setCedula(String cedula) {
-        this.cedula = cedula;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
     }
 
     public String getSexo() {
